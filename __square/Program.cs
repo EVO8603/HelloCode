620fdf10a1fa143ec5eb4be1aeebf108d8776367
EO <user8603@yandex.ru>
2021-11-28 18:45:24 +0900

    squair

---
+++ __square/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace My3
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               double x = 0.5;
+               int stepen = 4;
+               Console.WriteLine("0.5^4 = " + Math.Pow(x, stepen));
+               Console.WriteLine("5^4 = " + Math.Pow(5, 4));
+               Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
+               Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));
+               Console.ReadKey();
+          }
+     }
+}
