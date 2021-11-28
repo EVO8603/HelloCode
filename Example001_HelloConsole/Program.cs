30687cddb942e88d063f0c56dffd1a7075303cf6
EO <user8603@yandex.ru>
2021-11-28 18:40:26 +0900

    squair

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("2 Hello, World! ");
+﻿using System;
+
+namespace My3
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               double x = 0.5;
+               int stepen = 4;
+               Console.WriteLine("0.5^4 = " + Math.Pow(x, stepen));
+               Console.WriteLine("5^4 = " + Math.Pow(5, 4));
+               Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
+               Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));
+               Console.ReadKey();
+          }
+     }
+}
