bd671850a0d449bff9a90792d2ec3f94cb6deb43
EO <user8603@yandex.ru>
2021-11-28 20:17:41 +0900

    dayofweek

---
+++ __weekday/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApplication7
+{
+    public enum DayOfWeek
+    {
+        Понедельник = 0,
+        Вторник = 1,
+        Среда = 2,
+        Четверг = 3,
+        Пятниццо = 4,
+        Суббота = 5,
+        Воскресенье = 6
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("введите номер дня");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine((DayOfWeek)((n - 1) % Enum.GetValues(typeof(DayOfWeek)).Length));
+        }
+    }
+}
