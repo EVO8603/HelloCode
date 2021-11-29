641939e47506224e26d755e1a1257f709a5a5a0e
EO <user8603@yandex.ru>
2021-11-30 05:21:40 +0900

    Please enter the commit message for your changes. Lines starting

--- __isitsquare/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// проверяет, является ли число b квадратом числа a;
-int a = 3;
-int b = 8;
-if (b == a * a)
-Console.WriteLine("Yes, square");
-else
-Console.WriteLine("No ne square");
--- __square?/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿//Является ли число чётным?
-Console.WriteLine("Введите число: ");
-//double num = Convert.ToDouble (Console.ReadLine());
-//bool parity(double num) {// показывает истина или ложь
-//    return num % 2 == 0; //this method is uncorrect for negative numbers
-//}
-//Console.WriteLine(parity(num);
