8f4d7b554397e51d76796492f1cee49751c7e613
EO <user8603@yandex.ru>
2021-11-30 05:15:05 +0900

    squair

---
+++ 0__square/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace My3
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               double x = 3;
+               int stepen = 2;
+               Console.WriteLine(Math.Pow(x, stepen));
+               Console.ReadKey();
+          }
+     }
+}
---
+++ __max_min/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 6;
+int b = 5;
+if(a > b)
+     Console.WriteLine(a + " больше " + b);
+else 
+     Console.WriteLine(b  + " больше " + a);
