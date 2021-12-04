fbfa94242027b9d429f77c511cfcc9dc4bd14b76
EO <user8603@yandex.ru>
2021-12-04 13:47:39 +0900

    the larger figure

---
+++ 11__largestnumber/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Показывает наибольшую цифру числа
+Console.Clear();
+Console.WriteLine("Показывает наибольшее число");
+Console.WriteLine("Введите  число - ");
+int N = Convert.ToInt32 (Console.ReadLine());
+
+if (N % 10 >  N / 10)
+{
+    Console.WriteLine($"Наибольшая цифра числа {N} равна {N % 10}");   
+}else
+{
+    Console.WriteLine($"Наибольшая цифра числа {N} равна {N / 10}");
+}
