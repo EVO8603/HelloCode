a70fcbd7267d05df5cfbba04397d080506e9f165
EO <user8603@yandex.ru>
2021-12-06 05:58:14 +0900

    checking the passibility of dividing

---
+++ 13__kratnoilinet/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("input number for dividing");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input number ");
+int n = int.Parse(Console.ReadLine());
+
+int ost = N % n;
+if (ost == 0)
+{
+    Console.WriteLine("кратно");
+}
+
+else 
+{
+    Console.WriteLine($"остаток равен = {ost}");
+}
