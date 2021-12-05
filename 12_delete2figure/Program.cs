79c3fbc87c60f28de7070c9397553714603ae885
EO <user8603@yandex.ru>
2021-12-05 21:43:06 +0900

    delete second figure of number

---
+++ 12_delete2figure/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа 
+Console.WriteLine("Введите 3х значное число ");
+int n = int.Parse(Console.ReadLine());
+if (n<=999 & n>=100)
+{
+int first = n / 100; //находим первую цифру 3х значного числа
+int last = n % 10; // находим третью цифру 3х значного числа
+int fin = first*10 + last; // 1 2 3  >>>  1*10 = 10 >>>  10 + 3 = 13
+Console.WriteLine(fin);
+}
+else {
+    Console.WriteLine("your number is wrong");
+}
