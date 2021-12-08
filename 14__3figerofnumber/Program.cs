256d3cda10d6f64dcebd2a4750477eec9d42fb30
EO <user8603@yandex.ru>
2021-12-09 05:31:31 +0900

    3finger of number

---
+++ 14__3figerofnumber/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("input number");
+int a = int.Parse(Console.ReadLine());
+if (a <= 999 & a >=100)
+{
+    a = a % 10;
+    Console.WriteLine(a);
+}
+
+else if (a <= 99)
+{
+    Console.WriteLine("the third number is absent");
+}
+ else 
+ { 
+     while (a >999)
+     {
+         a = a/10;
+     }
+         a = a % 10;
+        Console.WriteLine(a);
+ }
