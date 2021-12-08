757d148554bb93425fae6d2bb322990295a6a4d3
EO <user8603@yandex.ru>
2021-12-09 06:36:25 +0900

    buleva algebra

---
+++ 18__proverkanaistinnost/Program.cs
@@ -0,0 +1,22 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+Console.WriteLine("Enter 2 numbers");
+
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if ((x == 0 | x == 1) & (y == 0 | y == 1))
+{
+    if ((-(x | y)) == (-x & -y))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+
+}
+else
+{
+    Console.WriteLine("неверные значения");
+}
