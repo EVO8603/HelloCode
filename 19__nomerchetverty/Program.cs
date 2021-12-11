a7ccdb70d9ad0a452063c17847a316f8064fa4fa
EO <user8603@yandex.ru>
2021-12-11 17:43:51 +0900

    write the number of quater

---
+++ 19__nomerchetverty/Program.cs
@@ -0,0 +1,20 @@
+﻿//Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+Console.WriteLine("input x&y");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+if (x > 0 & y > 0)
+{
+    Console.WriteLine("first quater");
+}
+else if (x < 0 & y > 0)
+    {
+        Console.WriteLine("second quater");
+    }
+else if (x < 0 & y < 0)
+    {
+        Console.WriteLine("third quater");
+    }
+else
+{
+    Console.WriteLine("fourth quater");
+}
