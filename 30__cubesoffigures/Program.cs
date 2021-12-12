0c5b7aeebdf087f140c1ccbfbf5d26e24b6a4ac1
EO <user8603@yandex.ru>
2021-12-13 06:22:44 +0900

    cubes of number if the last fugere is even

---
+++ 30__cubesoffigures/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+Console.WriteLine("введите число");
+
+double N = double.Parse(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    Console.WriteLine($"последняя цифpа числа {N} чётная, результат - {Math.Pow(N, 3)}");
+}
+else
+{
+    Console.WriteLine($"последняя цифpа числа {N} НЕ чётная");
+}
