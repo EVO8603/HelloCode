d03f1d804824c88b63fafd682127b3c1590178a2
EO <user8603@yandex.ru>
2021-12-13 05:44:06 +0900

    sum of figures

---
+++ 28__sumoffigures/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Ведите число");
+
+int figure = int.Parse(Console.ReadLine());
+int res = 0;
+int b=0;
+while (figure>0)
+{
+    b= figure % 10;
+    res = res+b; 
+    figure= figure/10;
+}
+Console.WriteLine(res);
