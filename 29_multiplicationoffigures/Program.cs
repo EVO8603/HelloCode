de1c61528dcf6cea1b7d780098537e69b46f0da4
EO <user8603@yandex.ru>
2021-12-13 06:05:46 +0900

    multiplication of figures

---
+++ 29_multiplicationoffigures/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("введите число");
+
+int N = int.Parse(Console.ReadLine());
+int res = 1;
+int i = 1;
+while (i <= N)
+{
+    res = res * i;
+    i++;
+
+}
+Console.WriteLine(res);
