c11d5e0e4cb0e2a4b83a5008f8dfcaf72b9a04ea
EO <user8603@yandex.ru>
2021-12-15 20:22:56 +0900

    array 8 elements

--- 31__massiv8elements/Program.cs
+++ 31__massiv8elements/Program.cs
@@ -1,11 +1,10 @@
 ﻿// Задать массив из 8 элементов и вывести их на экран
 Console.WriteLine("введите элементы массива");
 
-//int num = int.Parse(Console.ReadLine());
-
 int[] array = new int[8]; 
 int len = array.Length;
 int index = 0;
+
 while(index < len)
 {
     array[index] = int.Parse(Console.ReadLine());;
