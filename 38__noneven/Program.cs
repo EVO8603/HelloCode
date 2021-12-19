ea520e42c950ebefc35db5f12708b4a1793f521a
EO <user8603@yandex.ru>
2021-12-20 05:53:07 +0900

    write the number of quater

---
+++ 38__noneven/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.WriteLine("Enter the numbers");
+int[] array = new int[8];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+
+
+    array[i] = int.Parse(Console.ReadLine()); ;
+    sum = sum + array[i];
+    i++;
+
+}
+Console.WriteLine(sum);
