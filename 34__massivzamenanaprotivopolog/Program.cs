41b02e1c82edfdd1541d51c814566c26192f9f2c
EO <user8603@yandex.ru>
2021-12-20 05:53:41 +0900

    write the number of quater

--- 34__massivzamenanaprotivopolog/Program.cs
+++ 34__massivzamenanaprotivopolog/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Написать программу замену элементов массива на противоположные
-int[] array = new int[5];
+int[] array = new int[3];
 int len = array.Length;
 
 for (int i = 0; i < len; i++)
 {
-    array[i] = new Random().Next(-10, 10);
+    array[i] = new Random().Next(-100, 100);
     Console.Write($"{array[i]}  ");
 }
 Console.WriteLine(" ");
