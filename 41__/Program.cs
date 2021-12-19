0eefd620152d96fecc90cf83747bac668e839fca
EO <user8603@yandex.ru>
2021-12-20 06:04:57 +0900

    41

---
+++ 41__/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+double a = 7;  //new Random().Next(0,10);
+double b = 10;  //new Random().Next(0,10);
+double c = 1; //new Random().Next(0,10);
+
+if (a+b>c & a+c>b & b+c >a)
+{
+    Console.WriteLine("Три числа являются сторонами треугольника");
+}
+else
+{
+    Console.WriteLine("Три числа НЕ являются сторонами треугольника");
+}
